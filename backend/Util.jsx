b07400f5781f4779774389991198cc8b6c50a132
eddie <124751851+eddie505@users.noreply.github.com>
2024-06-07 00:48:34 -0600

    GITHUB BASED UPDATE

--- backend/Util.jsx
+++ backend/Util.jsx
@@ -1,4 +1,5 @@
 const REACT_APP_SERVER_URL =
-  process.env.REACT_APP_SERVER_URL || "http://192.168.155.120:4000";
+  process.env.REACT_APP_SERVER_URL ||
+  "https://bureau-manager-back-ow-production.up.railway.app";
 
 export { REACT_APP_SERVER_URL };
